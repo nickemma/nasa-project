125b501e3c87dc1286a910071a1f3be29dd2faae
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-08 22:44:32 +0100

    Fetched data from the server

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -3,12 +3,17 @@ import API_URL from '../config/api';
 // Load planets and return as JSON.
 const httpGetPlanets = async () => {
   const response = await fetch(`${API_URL}/planets`);
-  console.log(response);
   return await response.json();
 };
 
 // Load launches, sort by flight number, and return as JSON.
-const httpGetLaunches = async () => {};
+const httpGetLaunches = async () => {
+  const response = await fetch(`${API_URL}/launches`);
+  const launchedData = await response.json();
+  return launchedData.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+};
 
 // Submit given launch data to launch system.
 const httpSubmitLaunch = async (launch) => {};
